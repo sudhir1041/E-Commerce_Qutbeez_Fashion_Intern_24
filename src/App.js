b6574b25852ce38e4bb63316dfd5de2a9192fe23
kamil <kmlsddq53@gmail.com>
2024-07-02 12:24:08 +0530

    changes done by kamil to frontend

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./style.css";
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route>
+        Home
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>  
+  );
+};
+
+export default App;
